3d6f19256bd1e1fcca92f214e13813d989ca1be4
Lrini <iwangzr94@gmail.com>
2025-07-02 19:16:57 +0800

    laravel8.15 fix

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class registerController extends Controller
 {
@@ -13,11 +15,21 @@ class registerController extends Controller
     }
 
     public function store(Request $request){
-       $request->validate([
+      $validateData = $request->validate([
         'name' => 'required|max:30',
         'username' => 'required|max:30|min:3|unique:users',
-        'email' => 'required|email|unique:users',
+        'email' => 'required|email:dns|unique:users',
         'password' => 'required|min:5|max:255',
     ]);
+
+        // enkripsi password
+        //$validateData['password'] = bcrypt($validateData['password']);
+        $validateData['password'] = Hash::make($validateData['password']);
+        // buat user baru
+        // User::create($validateData); // cara ini tidak disarankan karena tidak aman
+        // lebih baik menggunakan create dengan guarded di model User
+    User::create($validateData);
+    //session()->flash('success', 'Registration successful! Please login');
+    return redirect('/login')->with('success', 'Registration successful! Please login');
     }
 }
